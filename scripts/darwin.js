ee9716b68330dd1892c2f3b7662c04623a36af6a
Shawn <sgraven66@gmail.com>
2024-03-25 11:55:53 -0500

    Added a reference to #59
    This is just a note of where to submit issues for MacOS

--- scripts/darwin.js
+++ scripts/darwin.js
@@ -1,2 +1,5 @@
 const {start} = require("./core.js")
+console.warn("Experamental and untested: Things may break")
+console.warn("Please submit all issues to https://github.com/zekrom-vale/electron-quick-start/issues/59")
+console.warn("Note: I have no plan on fixing issues for MacOS at this point")
 start("darwin", "x64")
